6c3310eac03cff15bbbca40a83da28b6f97c5b2d
Joy Bappy <joybappy2025@gmail.com>
2026-06-20 07:14:28 +0600

    try showmessageDialog 4 parameters

---
+++ programs/javaswing/Dialog/DialogMessage2.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+class DialogMessage2{
+    public static void main(String[]args){
+        // JOptionPane.showMessageDialog(positon, message, title, icon)
+        JOptionPane.showMessageDialog(null, "Are You Sure?", "CONFIRMATION", JOptionPane.QUESTION_MESSAGE);
+    }
+}
